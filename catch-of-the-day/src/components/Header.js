d0184354ada47b6f3f57129c5fae85ff4ab47a3b
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-07-01 15:20:24 -0700

    Lear about how props are accessed

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,10 @@ class Header extends React.Component {
 
   render() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Catch of the Day</h1>
+        <h3 className="tagline">{this.props.tagline}</h3>
+      </header>
     )
   }
 }
