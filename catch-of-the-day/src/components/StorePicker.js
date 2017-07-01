0175c63e122f62c616091a67a96080fe3adb8e01
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-07-01 14:29:13 -0700

    Add input text field and submit button

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello!</p>
+    // Make sure you don't fuck up a comment
+    // inside of JSX
+    //Don't put your comments at the top level
+    //You can only ever return one overall/ parent html element in JSX
+    return (
+      <form className="store-selector">
+        <h2> Please Enter A Store </h2>
+
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store 🍒 </button>
+      </form>
+    )
   }
 }
 
