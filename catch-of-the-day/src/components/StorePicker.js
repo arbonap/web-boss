041e0ac894c24da27743c3c1869ff363a5e9c298
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-07-01 13:31:44 -0700

    🎉 Initial commit. Create Store Picker component.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,17 +2,7 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    //Any where else!
-    return (
-      <form className="">
-      { /* Hello! Make sure to never put this
-        comment as the first element you return in your
-        return function. Put it nested within! */}
-      <h2>Please enter a store</h2>
-      <input type="text" placeholder="Store Name" />
-      <button type="submit">Visit Store</button>
-      </form>
-    )
+    return <p>Hello!</p>
   }
 }
 
